aa178a97bfcbc790ed33b6ed37ad323adc669b5e
Jason Carter <jcarter@naturalhues.com>
2021-09-26 23:29:24 -0400

    Added inv message

---
+++ src/main/kotlin/bitcoin/messages/InvMessage.kt
@@ -0,0 +1,73 @@
+package bitcoin.messages
+
+import bitcoin.messages.components.NetworkAddress
+import bitcoin.messages.components.VariableInt
+import bitcoin.messages.components.VariableString
+import util.ByteManipulation
+import java.nio.ByteOrder
+
+class InvMessage(
+    val transactionHashes: List<ByteArray>,
+    val dataBlockHashes: List<ByteArray>
+) {
+    fun toByteArray(): ByteArray {
+        val array = ByteArray(calculateMessageSize())
+        var currentOffset = 0
+
+        currentOffset = VariableInt(transactionHashes.size + dataBlockHashes.size.toLong()).intoByteArray(array, currentOffset)
+
+        for (hash in transactionHashes) {
+            currentOffset = ByteManipulation.writeIntToArray(1, array, currentOffset)
+            hash.copyInto(array, currentOffset)
+            currentOffset += hash.size
+        }
+
+        for (hash in dataBlockHashes) {
+            currentOffset = ByteManipulation.writeIntToArray(2, array, currentOffset)
+            hash.copyInto(array, currentOffset)
+            currentOffset += hash.size
+        }
+
+        return array
+    }
+
+    fun calculateMessageSize(): Int {
+        val numItems = VariableInt(transactionHashes.size + dataBlockHashes.size.toLong())
+        val numItemsSize = numItems.calculateMessageSize()
+        return numItemsSize + 36 * numItems.value.toInt()
+    }
+
+    companion object {
+        fun fromByteArray(buffer: ByteArray): InvMessage {
+            val numItemsInfo = VariableInt.fromByteArray(buffer, 0)
+            var currentOffset = numItemsInfo.calculateMessageSize()
+            val numItems = numItemsInfo.value
+
+            val transactionHashes = mutableListOf<ByteArray>()
+            val dataBlockHashes = mutableListOf<ByteArray>()
+
+            for (i in 0 until numItems) {
+                val typeData = ByteManipulation.readIntFromArray(buffer, currentOffset)
+                currentOffset = typeData.nextIndex
+                when (typeData.value) {
+                    1 -> {
+                        val hash = ByteArray(32)
+                        buffer.copyInto(hash, 0, currentOffset, currentOffset + 32)
+                        transactionHashes.add(hash)
+                    }
+                    2 -> {
+                        val hash = ByteArray(32)
+                        buffer.copyInto(hash, 0, currentOffset, currentOffset + 32)
+                        dataBlockHashes.add(hash)
+                    }
+                }
+                currentOffset += 32
+            }
+
+            return InvMessage(
+                transactionHashes = transactionHashes,
+                dataBlockHashes = dataBlockHashes
+            )
+        }
+    }
+}
---
+++ src/test/kotlin/bitcoin/messages/InvMessageTest.kt
@@ -0,0 +1,30 @@
+package bitcoin.messages
+
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+class InvMessageTest {
+    @Test
+    fun inAndOut_Normal_Success() {
+        val hash1 = createRandomHash()
+        val hash2 = createRandomHash()
+        val hash3 = createRandomHash()
+        val hash4 = createRandomHash()
+        val hash5 = createRandomHash()
+
+        val message = InvMessage(
+            transactionHashes = listOf(hash1, hash2, hash3),
+            dataBlockHashes = listOf(hash4, hash5)
+        )
+        val messageByteArray = message.toByteArray()
+        val outMessage = InvMessage.fromByteArray(messageByteArray)
+
+        assert(outMessage.transactionHashes[0].contentEquals(hash1))
+        assert(outMessage.transactionHashes[1].contentEquals(hash2))
+        assert(outMessage.transactionHashes[2].contentEquals(hash3))
+        assert(outMessage.dataBlockHashes[0].contentEquals(hash4))
+        assert(outMessage.dataBlockHashes[1].contentEquals(hash5))
+    }
+
+    private fun createRandomHash(): ByteArray = Random.Default.nextBytes(32)
+}
